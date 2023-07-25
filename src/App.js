db6b6d94eaeb61b9fa21144ed86bd7c518cddc96
Erik Van Hauthem <erik.vanhauthem@skynet.be>
2023-07-25 16:33:45 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
 				<p>Deployment is happening here !</p>
+				<p>Breaking Changes</p>
 				<p>By Erik</p>
 			</header>
 		</div>
